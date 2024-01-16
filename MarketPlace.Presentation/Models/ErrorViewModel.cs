5d5bf7f0ef8f6e6088a5ef129987820ef11f0336
AliBabarahaei <aliBabarahaei@gmail.com>
2024-01-16 22:02:26 +0330

    init project

---
+++ MarketPlace.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MarketPlace.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
