755bdc16b144172e077b600c85f52f11fce1b2d3
AliBabarahaei <aliBabarahaei@gmail.com>
2024-01-17 10:57:39 +0330

    Add ContactUs Model

---
+++ MarketPlace.Domain/Models/Contacts/ContactUs.cs
@@ -0,0 +1,44 @@
+﻿using MarketPlace.Domain.Models.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketPlace.Domain.Models.Contacts
+{
+    public class ContactUs : BaseEntity
+    {
+        #region properties
+
+        public long? UserId { get; set; }
+
+        [Display(Name = "IP کاربر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string UserIp { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "نام و نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string FullName { get; set; }
+
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Subject { get; set; }
+
+        [Display(Name = "متن پیام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Text { get; set; }
+
+        #endregion
+
+        #region relations
+
+       
+
+        #endregion
+    }
+}
