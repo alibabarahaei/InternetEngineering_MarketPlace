0ecd8081697ad2a7bfbba5ee6e480385cf94222c
AliBabarahaei <aliBabarahaei@gmail.com>
2024-01-17 10:55:07 +0330

    Create Product and BaseEntity DomainModel

---
+++ MarketPlace.Domain/Models/Products/Product.cs
@@ -0,0 +1,65 @@
+﻿using MarketPlace.Domain.Models.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketPlace.Domain.Models.Products
+{
+    public class Product : BaseEntity
+    {
+        #region properties
+
+        public long SellerId { get; set; }
+
+        [Display(Name = "نام محصول")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(300, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Title { get; set; }
+
+        [Display(Name = "تصویر محصول")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(300, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string ImageName { get; set; }
+
+        [Display(Name = "قیمت محصول")]
+        public int Price { get; set; }
+
+        [Display(Name = "توضیحات کوتاه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(500, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "توضیحات اصلی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Description { get; set; }
+
+        [Display(Name = "پیام تایید/عدم تایید")]
+        public string ProductAcceptOrRejectDescription { get; set; }
+
+        [Display(Name = "فعال / غیرفعال")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "وضعیت")]
+        public ProductAcceptanceState ProductAcceptanceState { get; set; }
+
+        [Display(Name = "درصد سایت")]
+        public int SiteProfit { get; set; }
+
+        #endregion
+
+        #region relations
+
+
+
+
+        #endregion
+    }
+
+    public enum ProductAcceptanceState
+    {
+        [Display(Name = "در حال بررسی")]
+        UnderProgress,
+        [Display(Name = "تایید شده")]
+        Accepted,
+        [Display(Name = "رد شده")]
+        Rejected
+    }
+}
