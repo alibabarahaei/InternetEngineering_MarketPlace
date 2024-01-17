91de9477d70b3218735d25162b44a3ee0ad15c15
AliBabarahaei <aliBabarahaei@gmail.com>
2024-01-17 10:59:10 +0330

    Add TicketMessage Model

---
+++ MarketPlace.Domain/Models/Contacts/TicketMessage.cs
@@ -0,0 +1,24 @@
+﻿using MarketPlace.Domain.Models.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketPlace.Domain.Models.Contacts
+{
+    public class TicketMessage : BaseEntity
+    {
+        #region properties
+
+        public long TicketId { get; set; }
+
+        public long SenderId { get; set; }
+
+        [Display(Name = "متن پیام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Text { get; set; }
+
+        #endregion
+
+        #region relations
+
+        #endregion
+    }
+}
