9c7835364fd2dca3583331c80c9d2a27d5dab617
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-17 18:02:49 +0330

    Structures of controller and areas

---
+++ MarketPlace.Presentation/Controllers/UploaderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Presentation.Controllers
+{
+    public class UploaderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
