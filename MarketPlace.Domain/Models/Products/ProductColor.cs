2094bb2834362fe6141619c51f83f38c5855a3aa
AliBabarahaei <aliBabarahaei@gmail.com>
2024-01-17 11:04:46 +0330

    add productcolor

---
+++ MarketPlace.Domain/Models/Products/ProductColor.cs
@@ -0,0 +1,31 @@
+﻿using MarketPlace.Domain.Models.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketPlace.Domain.Models.Products
+{
+    public class ProductColor : BaseEntity
+    {
+        #region properties
+
+        public long ProductId { get; set; }
+
+        [Display(Name = "رنگ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string ColorName { get; set; }
+
+        [Display(Name = "کد رنگ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string ColorCode { get; set; }
+
+        public int Price { get; set; }
+
+        #endregion
+
+        #region relations
+
+
+        #endregion
+    }
+}
