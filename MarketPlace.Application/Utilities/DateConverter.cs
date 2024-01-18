414914542e609b51c83411b4a7a57fd42176d981
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-18 22:44:04 +0330

    add date converter utility

---
+++ MarketPlace.Application/Utilities/DateConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace MarketPlace.Application.Utilities
+{
+    public static class DateConvertor
+    {
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            return persianCalendar.GetYear(value) + "/" +
+                   persianCalendar.GetMonth(value).ToString("00") + "/" +
+                   persianCalendar.GetDayOfMonth(value).ToString("00");
+        }
+    }
+}
