d1ee41238af734b41f5145e651f10a28b8008fd4
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-18 10:21:53 +0330

    add productdiscount model

---
+++ MarketPlace.Domain/Models/Products/ProductDiscount.cs
@@ -0,0 +1,31 @@
+﻿using MarketPlace.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Domain.Models.Products
+{
+    public class ProductDiscount : BaseEntity
+    {
+        #region properties
+
+        public long ProductId { get; set; }
+
+        [Range(0, 100)]
+        public int Percentage { get; set; }
+
+        public DateTime ExpireDate { get; set; }
+
+        public int DiscountNumber { get; set; }
+
+        #endregion
+
+        #region relations
+
+       
+        #endregion
+    }
+}
