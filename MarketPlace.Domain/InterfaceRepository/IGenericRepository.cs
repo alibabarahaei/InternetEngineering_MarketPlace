761626d99aab5b6a9ad91b2db2a238e8df78600b
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-18 22:56:26 +0330

    implement generic repository

---
+++ MarketPlace.Domain/InterfaceRepository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using MarketPlace.Domain.Models.Common;
+
+namespace MarketPlace.Domain.InterfaceRepository
+{
+    public interface IGenericRepository<TEntity> : IAsyncDisposable where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetQuery();
+        Task AddEntity(TEntity entity);
+        Task AddRangeEntities(List<TEntity> entities);
+        Task<TEntity> GetEntityById(long entityId);
+        void EditEntity(TEntity entity);
+        void DeleteEntity(TEntity entity);
+        Task DeleteEntity(long entityId);
+        void DeletePermanent(TEntity entity);
+        void DeletePermanentEntities(List<TEntity> entities);
+        Task DeletePermanent(long entityId);
+        Task SaveChanges();
+    }
+}
