fc18d5ce7ee49b5bf093d859674a6dada580748b
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-19 13:29:54 +0330

    add IdentityDbContext

--- MarketPlace.Infrastructure.EFCore/Context/InternetEngineeringMarketPlaceDbContext.cs
+++ MarketPlace.Infrastructure.EFCore/Context/InternetEngineeringMarketPlaceDbContext.cs
@@ -1,4 +1,60 @@
-﻿namespace MarketPlace.Infrastructure.EFCore.Context
-{
+﻿using MarketPlace.Domain.Models.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
+namespace MarketPlace.Infrastructure.EFCore.Context
+{
+    public class InternetEngineeringMarketPlaceDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public InternetEngineeringMarketPlaceDbContext(DbContextOptions<InternetEngineeringMarketPlaceDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region on model creating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(s => s.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+
+            base.OnModelCreating(modelBuilder);
+
+
+
+            #region ChangeTableName(Identity)
+            modelBuilder.Entity<ApplicationUser>().ToTable("Users").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+            #endregion
+
+
+        }
+        #endregion
+
+    }
 }
