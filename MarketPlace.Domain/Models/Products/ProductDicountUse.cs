eb6da3ae701271aaa423db19ce47c24c72dc69ef
mhb80 <Barati@mohammad>
2024-01-26 02:07:21 +0330

    disocoutuse and productdeatured updated

--- MarketPlace.Domain/Models/Products/ProductDicountUse.cs
+++ MarketPlace.Domain/Models/Products/ProductDicountUse.cs
@@ -8,13 +8,19 @@ namespace MarketPlace.Domain.Models.Products
 {
     public class ProductDiscountUse : BaseEntity
     {
+        #region properties
 
         public long ProductDiscountId { get; set; }
+
         public long UserId { get; set; }
+
+        #endregion
+
+        #region relations
 
         public User User { get; set; }
         public ProductDiscount ProductDiscount { get; set; }
 
-        
+        #endregion
     }
 }
