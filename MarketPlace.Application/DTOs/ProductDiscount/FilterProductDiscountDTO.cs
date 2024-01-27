74f090158f569406c59eef079674bc9c1689922d
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:02:25 +0330

    add product discount

---
+++ MarketPlace.Application/DTOs/ProductDiscount/FilterProductDiscountDTO.cs
@@ -0,0 +1,40 @@
+﻿using MarketPlace.Application.DTOs.Paging;
+
+namespace MarketPlace.Application.DTOs.ProductDiscount
+{
+    public class FilterProductDiscountDTO : BasePaging
+    {
+        #region properties
+
+        public long? ProductId { get; set; }
+
+        public long? SellerId { get; set; }
+
+        public List<Domain.Models.Products.ProductDiscount> ProductDiscounts { get; set; }
+
+        #endregion
+
+        #region methods
+
+        public FilterProductDiscountDTO SetDiscounts(List<Domain.Models.Products.ProductDiscount> productDiscounts)
+        {
+            this.ProductDiscounts = productDiscounts;
+            return this;
+        }
+
+        public FilterProductDiscountDTO SetPaging(BasePaging paging)
+        {
+            this.PageId = paging.PageId;
+            this.AllEntitiesCount = paging.AllEntitiesCount;
+            this.StartPage = paging.StartPage;
+            this.EndPage = paging.EndPage;
+            this.HowManyShowPageAfterAndBefore = paging.HowManyShowPageAfterAndBefore;
+            this.TakeEntity = paging.TakeEntity;
+            this.SkipEntity = paging.SkipEntity;
+            this.PageCount = paging.PageCount;
+            return this;
+        }
+
+        #endregion
+    }
+}
