d64c341b16dec97859da9347678f3133f5fa5e81
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:00:30 +0330

    add orders dto

---
+++ MarketPlace.Application/DTOs/Orders/UserOpenOrderDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace MarketPlace.Application.DTOs.Orders
+{
+    public class UserOpenOrderDTO
+    {
+        public long UserId { get; set; }
+
+        public string Description { get; set; }
+
+        public List<UserOpenOrderDetailItemDTO> Details { get; set; }
+
+        public int GetTotalPrice()
+        {
+            return Details.Sum(s => (s.ProductPrice + s.ProductColorPrice) * s.Count);
+        }
+
+        public int GetTotalDiscounts()
+        {
+            return Details.Sum(s => s.GetOrderDetailWithDiscountPriceAmount());
+        }
+    }
+}
