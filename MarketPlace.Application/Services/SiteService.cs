668f43e7c22157f4ca66efd6cd38840ef26ed684
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:15:14 +0330

    add SiteService

---
+++ MarketPlace.Application/Services/SiteService.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarketPlace.Application.Services.Interfaces;
+using MarketPlace.DataLayer.Entities.Site;
+using MarketPlace.DataLayer.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Application.Services.Implementations
+{
+    public class SiteService : ISiteService
+    {
+        #region constructor
+
+        private readonly IGenericRepository<SiteSetting> _siteSettingRepository;
+        private readonly IGenericRepository<Slider> _sliderRepository;
+        private readonly IGenericRepository<SiteBanner> _bannerRepository;
+
+        public SiteService(IGenericRepository<SiteSetting> siteSettingRepository, IGenericRepository<Slider> sliderRepository, IGenericRepository<SiteBanner> bannerRepository)
+        {
+            _siteSettingRepository = siteSettingRepository;
+            _sliderRepository = sliderRepository;
+            _bannerRepository = bannerRepository;
+        }
+
+        #endregion
+
+        #region site settings
+
+        public async Task<SiteSetting> GetDefaultSiteSetting()
+        {
+            return await _siteSettingRepository.GetQuery().AsQueryable()
+                .SingleOrDefaultAsync(s => s.IsDefault && !s.IsDelete);
+        }
+
+        #endregion
+
+        #region slider
+
+        public async Task<List<Slider>> GetAllActiveSliders()
+        {
+            return await _sliderRepository.GetQuery().AsQueryable()
+                .Where(s => s.IsActive && !s.IsDelete).ToListAsync();
+        }
+
+        #endregion
+
+        #region site banners
+
+        public async Task<List<SiteBanner>> GetSiteBannersByPlacement(List<BannerPlacement> placements)
+        {
+            return await _bannerRepository.GetQuery().AsQueryable()
+                .Where(s => placements.Contains(s.BannerPlacement)).ToListAsync();
+        }
+
+        #endregion
+
+        #region dispose
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_siteSettingRepository != null) await _siteSettingRepository.DisposeAsync();
+            if (_sliderRepository != null) await _sliderRepository.DisposeAsync();
+            if (_bannerRepository != null) await _bannerRepository.DisposeAsync();
+        }
+
+        #endregion
+    }
+}
