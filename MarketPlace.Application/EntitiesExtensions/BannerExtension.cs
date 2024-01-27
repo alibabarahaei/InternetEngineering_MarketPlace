b0c65ff529036f1e825a0b0c1de015e458eaf192
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:22:27 +0330

    add entity extensions

---
+++ MarketPlace.Application/EntitiesExtensions/BannerExtension.cs
@@ -0,0 +1,13 @@
+﻿using MarketPlace.Application.Utilities;
+using MarketPlace.Domain.Models.Site;
+
+namespace MarketPlace.Application.EntitiesExtensions
+{
+    public static class BannerExtension
+    {
+        public static string GetBannerMainImageAddress(this SiteBanner banner)
+        {
+            return PathExtension.BannerOrigin + banner.ImageName;
+        }
+    }
+}
