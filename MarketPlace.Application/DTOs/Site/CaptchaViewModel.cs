bf2c70b9d4a2dba9cb37e439fa439b81df772e3e
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:05:18 +0330

    add site dto

---
+++ MarketPlace.Application/DTOs/Site/CaptchaViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketPlace.Application.DTOs.Site
+{
+    public class CaptchaViewModel
+    {
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Captcha { get; set; }
+    }
+}
