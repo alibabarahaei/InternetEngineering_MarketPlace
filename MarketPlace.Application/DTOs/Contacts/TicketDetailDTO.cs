06c34e2f11130aea7fe4afd04b58cdf59df30131
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 22:59:39 +0330

    add contacts dto

---
+++ MarketPlace.Application/DTOs/Contacts/TicketDetailDTO.cs
@@ -0,0 +1,11 @@
+﻿using MarketPlace.Domain.Models.Contacts;
+
+namespace MarketPlace.Application.DTOs.Contacts
+{
+    public class TicketDetailDTO
+    {
+        public Ticket Ticket { get; set; }
+
+        public List<TicketMessage> TicketMessages { get; set; }
+    }
+}
