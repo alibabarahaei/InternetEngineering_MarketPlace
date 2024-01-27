7fe5d30f7851eff730853e4ad6573d0d7d8febc5
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 22:57:33 +0330

    add account dto

---
+++ MarketPlace.Application/DTOs/Account/ForgotPasswordDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketPlace.Application.DTOs.Account
+{
+    public class ForgotPasswordDTO : CaptchaViewModel
+    {
+        [Display(Name = "تلفن همراه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Mobile { get; set; }
+    }
+
+    public enum ForgotPasswordResult
+    {
+        Success,
+        NotFound,
+        Error
+    }
+}
---
+++ MarketPlace.Application/InterfaceServices/IContactService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Application.InterfaceServices
+{
+    public interface IContactService : IAsyncDisposable
+    {
+        #region contact us
+
+        Task CreateContactUs(CreateContactUsDTO contact, string userIp, long? userId);
+
+
+        #endregion
+
+        #region ticket
+
+        Task<AddTicketResult> AddUserTicket(AddTicketDTO ticket, long userId);
+        Task<FilterTicketDTO> FilterTickets(FilterTicketDTO filter);
+        Task<TicketDetailDTO> GetTicketForShow(long ticketId, long userId);
+        Task<AnswerTicketResult> AnswerTicket(AnswerTicketDTO answer, long userId);
+
+        #endregion
+    }
+}
