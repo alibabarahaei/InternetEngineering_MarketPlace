f8e316cc0c5976b6bfbc4d8a99584e287778c069
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:08:22 +0330

    add ISellerWalletService

---
+++ MarketPlace.Application/InterfaceServices/ISellerWalletService.cs
@@ -0,0 +1,15 @@
+﻿using MarketPlace.Application.DTOs.SellerWallet;
+using MarketPlace.Domain.Models.Wallet;
+
+namespace MarketPlace.Application.InterfaceServices
+{
+    public interface ISellerWalletService
+    {
+        #region wallet
+
+        Task<FilterSellerWalletDTO> FilterSellerWallet(FilterSellerWalletDTO filter);
+        Task AddWallet(SellerWallet wallet);
+
+        #endregion
+    }
+}
