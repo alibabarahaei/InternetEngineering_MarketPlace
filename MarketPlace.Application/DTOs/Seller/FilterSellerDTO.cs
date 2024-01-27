731f26aba9af193e637f9967b72934df7bfd23dd
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:04:56 +0330

    add sellerwallet

---
+++ MarketPlace.Application/DTOs/Seller/FilterSellerDTO.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MarketPlace.Application.DTOs.Paging;
+
+namespace MarketPlace.Application.DTOs.Seller
+{
+    public class FilterSellerDTO : BasePaging
+    {
+        #region properties
+
+        public long? UserId { get; set; }
+        public string StoreName { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public FilterSellerState State { get; set; }
+
+        public List<Domain.Models.Store.Seller> Sellers { get; set; }
+
+        #endregion
+
+
+        #region methods
+
+        public FilterSellerDTO SetSellers(List<Domain.Models.Store.Seller> sellers)
+        {
+            this.Sellers = sellers;
+            return this;
+        }
+
+        public FilterSellerDTO SetPaging(BasePaging paging)
+        {
+            this.PageId = paging.PageId;
+            this.AllEntitiesCount = paging.AllEntitiesCount;
+            this.StartPage = paging.StartPage;
+            this.EndPage = paging.EndPage;
+            this.HowManyShowPageAfterAndBefore = paging.HowManyShowPageAfterAndBefore;
+            this.TakeEntity = paging.TakeEntity;
+            this.SkipEntity = paging.SkipEntity;
+            this.PageCount = paging.PageCount;
+            return this;
+        }
+
+        #endregion
+    }
+
+    public enum FilterSellerState
+    {
+        [Display(Name = "همه")]
+        All,
+        [Display(Name = "در حال بررسی")]
+        UnderProgress,
+        [Display(Name = "تایید شده")]
+        Accepted,
+        [Display(Name = "رد شده")]
+        Rejected
+    }
+}
---
+++ MarketPlace.Application/DTOs/SellerWallet/FilterSellerWalletDTO.cs
@@ -0,0 +1,35 @@
+﻿using MarketPlace.Application.DTOs.Paging;
+
+namespace MarketPlace.Application.DTOs.SellerWallet
+{
+    public class FilterSellerWalletDTO : BasePaging
+    {
+        public long? SellerId { get; set; }
+
+        public int? PriceFrom { get; set; }
+
+        public int? PriceTo { get; set; }
+
+        public List<Domain.Models.Wallet.SellerWallet> SellerWallets { get; set; }
+
+        public FilterSellerWalletDTO SetSellerWallets(List<Domain.Models.Wallet.SellerWallet> wallets)
+        {
+            SellerWallets = wallets;
+            return this;
+        }
+
+        public FilterSellerWalletDTO SetPaging(BasePaging paging)
+        {
+            this.PageId = paging.PageId;
+            this.AllEntitiesCount = paging.AllEntitiesCount;
+            this.StartPage = paging.StartPage;
+            this.EndPage = paging.EndPage;
+            this.HowManyShowPageAfterAndBefore = paging.HowManyShowPageAfterAndBefore;
+            this.TakeEntity = paging.TakeEntity;
+            this.SkipEntity = paging.SkipEntity;
+            this.PageCount = paging.PageCount;
+
+            return this;
+        }
+    }
+}
