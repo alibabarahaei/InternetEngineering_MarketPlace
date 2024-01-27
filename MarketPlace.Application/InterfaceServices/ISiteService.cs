a299818bfc2872e92ce9caea353242b839bc382d
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:08:43 +0330

    add ISiteService

---
+++ MarketPlace.Application/InterfaceServices/ISiteService.cs
@@ -0,0 +1,25 @@
+﻿using MarketPlace.Domain.Models.Site;
+
+namespace MarketPlace.Application.InterfaceServices
+{
+    public interface ISiteService : IAsyncDisposable
+    {
+        #region site settings
+
+        Task<SiteSetting> GetDefaultSiteSetting();
+
+        #endregion
+
+        #region slider
+
+        Task<List<Slider>> GetAllActiveSliders();
+
+        #endregion
+
+        #region site banners
+
+        Task<List<SiteBanner>> GetSiteBannersByPlacement(List<BannerPlacement> placements);
+
+        #endregion
+    }
+}
