ea2e98aede76ecad648732c9d7c4dcd7d69df55f
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:08:04 +0330

    add ISellerService

---
+++ MarketPlace.Application/InterfaceServices/ISellerService.cs
@@ -0,0 +1,22 @@
+﻿using MarketPlace.Application.DTOs.Common;
+using MarketPlace.Application.DTOs.Seller;
+using MarketPlace.Domain.Models.Store;
+
+namespace MarketPlace.Application.InterfaceServices
+{
+    public interface ISellerService : IAsyncDisposable
+    {
+        #region seller
+
+        Task<RequestSellerResult> AddNewSellerRequest(RequestSellerDTO seller, long userId);
+        Task<FilterSellerDTO> FilterSellers(FilterSellerDTO filter);
+        Task<EditRequestSellerDTO> GetRequestSellerForEdit(long id, long currentUserId);
+        Task<EditRequestSellerResult> EditRequestSeller(EditRequestSellerDTO request, long currentUserId);
+        Task<bool> AcceptSellerRequest(long requestId);
+        Task<bool> RejectSellerRequest(RejectItemDTO reject);
+        Task<Seller> GetLastActiveSellerByUserId(long userId);
+        Task<bool> HasUserAnyActiveSellerPanel(long userId);
+
+        #endregion
+    }
+}
