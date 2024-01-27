129fb050744867e3c679b247e6c711cae6d1507a
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:05:57 +0330

    add IOrderService

---
+++ MarketPlace.Application/InterfaceServices/IOrderService.cs
@@ -0,0 +1,23 @@
+﻿namespace MarketPlace.Application.InterfaceServices
+{
+    public interface IOrderService : IAsyncDisposable
+    {
+        #region order
+
+        Task<long> AddOrderForUser(long userId);
+        Task<Order> GetUserLatestOpenOrder(long userId);
+        Task<int> GetTotalOrderPriceForPayment(long userId);
+        Task PayOrderProductPriceToSeller(long userId, long refId);
+
+        #endregion
+
+        #region order detail
+
+        Task ChangeOrderDetailCount(long detailId, long userId, int count);
+        Task AddProductToOpenOrder(long userId, AddProductToOrderDTO order);
+        Task<UserOpenOrderDTO> GetUserOpenOrderDetail(long userId);
+        Task<bool> RemoveOrderDetail(long detailId, long userId);
+
+        #endregion
+    }
+}
