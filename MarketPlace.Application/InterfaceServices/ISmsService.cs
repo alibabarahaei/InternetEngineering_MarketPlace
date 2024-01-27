6e21e5fc5424a040f76c3707b411c2183695d519
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-27 23:08:57 +0330

    add ISmsService

---
+++ MarketPlace.Application/InterfaceServices/ISmsService.cs
@@ -0,0 +1,8 @@
+﻿namespace MarketPlace.Application.InterfaceServices
+{
+    public interface ISmsService
+    {
+        Task SendVerificationSms(string mobile, string activationCode);
+        Task SendUserPasswordSms(string mobile, string password);
+    }
+}
