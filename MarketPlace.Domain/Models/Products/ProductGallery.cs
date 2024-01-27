441d627ede77842e5f56511135d961f21da45965
mhb80 <Barati@mohammad>
2024-01-27 17:19:20 +0330

    productgallerycategory & product selected updated

--- MarketPlace.Domain/Models/Products/ProductGallery.cs
+++ MarketPlace.Domain/Models/Products/ProductGallery.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace MarketPlace.Domain.Models.Products
 {
-    internal class ProductGallery
+    public class ProductGallery : BaseEntity
     {
+        #region properties
+
+        public long ProductId { get; set; }
+
+        public int DisplayPriority { get; set; }
+
+        [Display(Name = "نام تصویر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [MaxLength(300, ErrorMessage = "{0} نمیتواند بیشتر از {1} کاراکتر باشد")]
+        public string ImageName { get; set; }
+
+        #endregion
+
+        #region relations
+
+        public Product Product { get; set; }
+
+        #endregion
     }
 }
