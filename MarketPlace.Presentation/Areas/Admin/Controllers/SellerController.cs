38f0f2faadb0ad722e18baa4b1fdc9d2b331a3ed
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-28 21:45:06 +0330

    add admin controller

---
+++ MarketPlace.Presentation/Areas/Admin/Controllers/SellerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Presentation.Areas.Admin.Controllers
+{
+    public class SellerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
