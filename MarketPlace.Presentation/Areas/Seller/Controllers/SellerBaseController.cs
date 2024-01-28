95055ba96e73a812e089edf238612386656c7a9f
AliBabarahaei <AliBabarahaei@gmail.com>
2024-01-28 21:47:22 +0330

    add seller controller

---
+++ MarketPlace.Presentation/Areas/Seller/Controllers/SellerBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Presentation.Areas.Seller.Controllers
+{
+    public class SellerBaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MarketPlace.Presentation/Areas/Seller/Controllers/SellerWalletController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Presentation.Areas.Seller.Controllers
+{
+    public class SellerWalletController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
