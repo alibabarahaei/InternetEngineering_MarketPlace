4eee7a9497d56f39797e792083f312fe2b0d948f
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:31:55 +0330

    Add SellerSidebarViewComponent Seller

---
+++ MarketPlace.Presentation/Areas/Seller/ViewComponents/SellerSidebarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.Areas.Seller.ViewComponents
+{
+    public class SellerSidebarViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SellerSidebar");
+        }
+    }
+}
