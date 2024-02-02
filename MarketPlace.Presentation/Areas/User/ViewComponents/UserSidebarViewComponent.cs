f1972c31f4d246d38f7ce1b7144063253c3b3ef8
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:59:36 +0330

    Add UserSidebarViewComponent

---
+++ MarketPlace.Presentation/Areas/User/ViewComponents/UserSidebarViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using MarketPlace.Application.InterfaceServices;
+using MarketPlace.Presentation.PresentationExtensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.Areas.User.ViewComponents
+{
+    public class UserSidebarViewComponent : ViewComponent
+    {
+        private ISellerService _sellerService;
+
+        public UserSidebarViewComponent(ISellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.hasUserAnyActiveSellerPanel = await _sellerService.HasUserAnyActiveSellerPanel(User.GetUserId());
+
+            return View("UserSidebar");
+        }
+    }
+}
