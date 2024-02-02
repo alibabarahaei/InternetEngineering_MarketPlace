ca118ef7e26eea4f2c3b1cde5d39ddc917de1100
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:00:54 +0330

    Edit Product Controller

--- MarketPlace.Presentation/Controllers/ProductController.cs
+++ MarketPlace.Presentation/Controllers/ProductController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketPlace.Application.DTOs.Products;
+using MarketPlace.Application.InterfaceServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MarketPlace.Presentation.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : SiteBaseController
     {
-        public IActionResult Index()
+        #region constructor
+
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
         {
-            return View();
+            _productService = productService;
         }
+
+        #endregion
+
+        #region filter products
+
+        [HttpGet("products")]
+        [HttpGet("products/{Category}")]
+        public async Task<IActionResult> FilterProducts(FilterProductDTO filter)
+        {
+            filter.TakeEntity = 9;
+            filter.FilterProductState = FilterProductState.Accepted;
+            filter = await _productService.FilterProducts(filter);
+            ViewBag.ProductCategories = await _productService.GetAllActiveProductCategories();
+            if (filter.PageId > filter.GetLastPage() && filter.GetLastPage() != 0) return NotFound();
+            return View(filter);
+        }
+
+        #endregion
+
+        #region show product detail
+
+        [HttpGet("products/{productId}/{title}")]
+        public async Task<IActionResult> ProductDetail(long productId, string title)
+        {
+            var product = await _productService.GetProductDetailById(productId);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+
+        #endregion
     }
 }
