9943f37e7b070d3cf7a6b7e4ab77fb47bb2268a5
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:31:12 +0330

    Add SellerBaseController Seller

--- MarketPlace.Presentation/Areas/Seller/Controllers/SellerBaseController.cs
+++ MarketPlace.Presentation/Areas/Seller/Controllers/SellerBaseController.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketPlace.Presentation.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MarketPlace.Presentation.Areas.Seller.Controllers
 {
+    [Authorize]
+    [Area("Seller")]
+    [Route("seller")]
+    [CheckSellerState]
     public class SellerBaseController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        protected string ErrorMessage = "ErrorMessage";
+        protected string SuccessMessage = "SuccessMessage";
+        protected string InfoMessage = "InfoMessage";
+        protected string WarningMessage = "WarningMessage";
     }
 }
