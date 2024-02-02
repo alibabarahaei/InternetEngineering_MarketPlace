df8f4522b0c4d632a8a3aa0c53f043229cd3e5a3
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:01:18 +0330

    Edit Upload Controller

--- MarketPlace.Presentation/Controllers/UploaderController.cs
+++ MarketPlace.Presentation/Controllers/UploaderController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketPlace.Application.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MarketPlace.Presentation.Controllers
 {
-    public class UploaderController : Controller
+    public class UploaderController : SiteBaseController
     {
-        public IActionResult Index()
+
+        [HttpPost]
+        public IActionResult UploadImage(IFormFile upload, string CKEditorFuncName, string CKEditor, string langCode)
         {
-            return View();
+            if (upload.Length <= 0) return null;
+            if (!upload.IsImage())
+            {
+                var notImageMessage = "لطفا یک تصویر انتخاب کنید";
+                var notImage = JsonConvert.DeserializeObject("{'uploaded':0, 'error': {'message': \" " + notImageMessage + " \"}}");
+                return Json(notImage);
+            }
+
+            var fileName = Guid.NewGuid() + Path.GetExtension(upload.FileName).ToLower();
+            upload.AddImageToServer(fileName, PathExtension.UploadImageServer, null, null);
+            return Json(new
+            {
+                uploaded = true,
+                url = $"{PathExtension.UploadImage}{fileName}"
+            });
         }
     }
 }
