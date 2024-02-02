be6cc172536f83ac2f065ca8db3647753295ff59
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:21:19 +0330

    Add SellerController Admin

--- MarketPlace.Presentation/Areas/Admin/Controllers/SellerController.cs
+++ MarketPlace.Presentation/Areas/Admin/Controllers/SellerController.cs
@@ -1,12 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketPlace.Application.DTOs.Common;
+using MarketPlace.Application.DTOs.Seller;
+using MarketPlace.Application.InterfaceServices;
+using MarketPlace.Presentation.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MarketPlace.Presentation.Areas.Admin.Controllers
 {
-    public class SellerController : Controller
+    public class SellerController : AdminBaseController
     {
-        public IActionResult Index()
+        #region constructor
+
+        private readonly ISellerService _sellerService;
+
+        public SellerController(ISellerService sellerService)
         {
-            return View();
+            _sellerService = sellerService;
         }
+
+        #endregion
+
+        #region seller requests
+
+        public async Task<IActionResult> SellerRequests(FilterSellerDTO filter)
+        {
+            return View(await _sellerService.FilterSellers(filter));
+        }
+
+        #endregion
+
+        #region accept seller request
+
+        public async Task<IActionResult> AcceptSellerRequest(long requestId)
+        {
+            var result = await _sellerService.AcceptSellerRequest(requestId);
+
+            if (result)
+            {
+                return JsonResponseStatus.SendStatus(
+                    JsonResponseStatusType.Success,
+                    "درخواست مورد نظر با موفقیت تایید شد",
+                    null);
+            }
+
+            return JsonResponseStatus.SendStatus(JsonResponseStatusType.Danger,
+                "اطلاعاتی با این مشخصات یافت نشد", null);
+        }
+
+        #endregion
+
+        #region reject seller request
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> RejectSellerRequest(RejectItemDTO reject)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _sellerService.RejectSellerRequest(reject);
+
+                if (result)
+                {
+                    return JsonResponseStatus.SendStatus(
+                        JsonResponseStatusType.Success,
+                        "درخواست مورد نظر با موفقیت رد شد",
+                        reject);
+                }
+            }
+
+            return JsonResponseStatus.SendStatus(JsonResponseStatusType.Danger,
+                "اطلاعاتی با این مشخصات یافت نشد", null);
+        }
+
+        #endregion
     }
 }
