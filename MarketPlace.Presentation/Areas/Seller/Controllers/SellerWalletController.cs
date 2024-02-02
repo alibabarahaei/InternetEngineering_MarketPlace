1864266c5f377fd6e0c8636300a9e0403a312984
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:31:30 +0330

    Add SellerWalletController Seller

--- MarketPlace.Presentation/Areas/Seller/Controllers/SellerWalletController.cs
+++ MarketPlace.Presentation/Areas/Seller/Controllers/SellerWalletController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketPlace.Application.DTOs.SellerWallet;
+using MarketPlace.Application.InterfaceServices;
+using MarketPlace.Presentation.PresentationExtensions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MarketPlace.Presentation.Areas.Seller.Controllers
 {
-    public class SellerWalletController : Controller
+    public class SellerWalletController : SellerBaseController
     {
-        public IActionResult Index()
+        #region constructor
+
+        private readonly ISellerWalletService _sellerWalletService;
+        private readonly ISellerService _sellerService;
+
+        public SellerWalletController(ISellerWalletService sellerWalletService, ISellerService sellerService)
         {
-            return View();
+            _sellerWalletService = sellerWalletService;
+            _sellerService = sellerService;
         }
+
+        #endregion
+
+        #region index
+
+        [HttpGet("seller-wallet")]
+        public async Task<IActionResult> Index(FilterSellerWalletDTO filter)
+        {
+            filter.TakeEntity = 5;
+            var seller = await _sellerService.GetLastActiveSellerByUserId(User.GetUserId());
+            if (seller == null) return NotFound();
+            filter.SellerId = seller.Id;
+            return View(await _sellerWalletService.FilterSellerWallet(filter));
+        }
+
+        #endregion
     }
 }
