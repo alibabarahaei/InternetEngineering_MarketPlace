d9b10cc13f7c12ad89244af7caf6cab2d1ffe222
AliBabarahaei <AliBabarahaei@gmail.com>
2024-02-02 20:20:53 +0330

    Add ProductController Admin

--- MarketPlace.Presentation/Areas/Admin/Controllers/ProductController.cs
+++ MarketPlace.Presentation/Areas/Admin/Controllers/ProductController.cs
@@ -1,12 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketPlace.Application.DTOs.Common;
+using MarketPlace.Application.DTOs.Products;
+using MarketPlace.Application.InterfaceServices;
+using MarketPlace.Presentation.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MarketPlace.Presentation.Areas.Admin.Controllers
 {
-    public class ProductController : Controller
+    public class ProductsController : AdminBaseController
     {
-        public IActionResult Index()
+        #region constructor
+
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
         {
-            return View();
+            _productService = productService;
         }
+
+        #endregion
+
+        #region filter products
+
+        public async Task<IActionResult> Index(FilterProductDTO filter)
+        {
+            return View(await _productService.FilterProducts(filter));
+        }
+
+        #endregion
+
+        #region accept product
+
+        public async Task<IActionResult> AcceptSellerProduct(long id)
+        {
+            var result = await _productService.AcceptSellerProduct(id);
+            if (result)
+            {
+                return JsonResponseStatus.SendStatus(JsonResponseStatusType.Success, "محصول مورد نظر با موفقیت تایید شد", null);
+            }
+
+            return JsonResponseStatus.SendStatus(JsonResponseStatusType.Danger, "محصول مورد نظر یافت نشد", null);
+        }
+
+        #endregion
+
+        #region reject product
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> RejectProduct(RejectItemDTO reject)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _productService.RejectSellerProduct(reject);
+
+                if (result)
+                {
+
+                    return JsonResponseStatus.SendStatus(JsonResponseStatusType.Success,
+                        "محصول مورد نظر با موفقیت رد شد", reject);
+                }
+
+                return JsonResponseStatus.SendStatus(JsonResponseStatusType.Danger, "اطلاعات مورد نظر جهت عدم تایید را به درستی وارد نمایید", null);
+            }
+
+
+            return JsonResponseStatus.SendStatus(JsonResponseStatusType.Danger, "محصول مورد نظر یافت نشد", null);
+        }
+
+        #endregion
     }
 }
